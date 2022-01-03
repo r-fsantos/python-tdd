b5509e9cb1a40a89d823010255566efa30ca5867
renato <felicio95.jr@gmail.com>
2022-01-02 23:11:22 -0300

    :recycle: Splitting employer costs among employer types.
    Tests :white_check_mark:

--- src/entities/employee.py
+++ src/entities/employee.py
@@ -17,7 +17,9 @@ class Employee:
 	employee_id: int
 	pay_rate: float = 100.0
 	hours_worked: float = 0.0
-	employer_cost: float = 1000.0
+	employer_office_costs: float = 200.0
+	employer_401k_costs: float = 400.0
+	employer_support_costs: float = 400.0
 	has_comission: bool = True
 	comission: float = 100.0
 	contracts_landed: int = 0
@@ -25,7 +27,14 @@ class Employee:
 	def compute_payout(self) -> float:
 		"""Compute how much must be paid to an employee"""
 		payout: float = self.pay_rate * self.hours_worked
-		payout += self.employer_cost
+
+		employer_cost: float = (
+			self.employer_office_costs
+			+ self.employer_support_costs
+			+ self.employer_401k_costs
+		)
+
+		payout += employer_cost
 
 		if self.has_comission:
 			payout += self.comission * self.contracts_landed
